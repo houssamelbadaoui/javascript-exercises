9ad58b187f738328acb22ad3fe9aa559ee6e83b9
houssamelbadaoui <houssamelbadaoui5@gmail.com>
2025-12-22 23:51:26 +0100

    leapYears solved

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+   let YearDivisibleByFour = year % 4 === 0;
+   let YearDivisibleByHundred = year % 100 === 0;
+   let YearDivisibleByFourHundred = year % 400 === 0;
+   if(YearDivisibleByFour &&
+    (!YearDivisibleByHundred || YearDivisibleByFourHundred)
+   ){
+    return true;
+   }else{
+    return false;
+   }
 };
 
 // Do not edit below this line
